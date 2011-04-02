de9ce23159d36df71143c550e7310e883a9a10e3
willmot <willmot@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-04-02 19:56:02 +0000

    1.1.4 Bug fix release.
    git-svn-id: https://plugins.svn.wordpress.org/backupwordpress/tags/1.1.4@368222 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin.actions.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * Delete the backup and then redirect
+ * back to the backups page
+ */
+function hmbkp_request_delete_backup() {
+
+	if ( !isset( $_GET['hmbkp_delete'] ) || empty( $_GET['hmbkp_delete'] ) )
+		return false;
+
+	hmbkp_delete_backup( $_GET['hmbkp_delete'] );
+
+	wp_redirect( remove_query_arg( 'hmbkp_delete' ) );
+	exit;
+
+}
+add_action( 'load-tools_page_' . HMBKP_PLUGIN_SLUG, 'hmbkp_request_delete_backup' );
+
+/**
+ * Schedule a one time backup and then
+ * redirect back to the backups page
+ */
+function hmbkp_request_do_backup() {
+
+	// Are we sure
+	if ( !isset( $_GET['action'] ) || $_GET['action'] !== 'hmbkp_backup_now' || hmbkp_is_in_progress() || !is_writable( hmbkp_path() ) || !is_dir( hmbkp_path() ) )
+		return false;
+	
+	// Schedule a single backup
+	wp_schedule_single_event( time(), 'hmbkp_schedule_single_backup_hook' );
+	
+	// Fire the cron now
+	wp_cron();
+	
+	// Redirect back
+	wp_redirect( remove_query_arg( 'action' ) );
+	exit;
+
+}
+add_action( 'load-tools_page_' . HMBKP_PLUGIN_SLUG, 'hmbkp_request_do_backup' );
+
+/**
+ * Send the download file to the browser and
+ * then redirect back to the backups page
+ */
+function hmbkp_request_download_backup() {
+
+	if ( !isset( $_GET['hmbkp_download'] ) || empty( $_GET['hmbkp_download'] ) )
+		return false;
+
+	hmbkp_send_file( base64_decode( $_GET['hmbkp_download'] ) );
+
+}
+add_action( 'load-tools_page_' . HMBKP_PLUGIN_SLUG, 'hmbkp_request_download_backup' );
+
+function hmbkp_ajax_is_backup_in_progress() {
+	echo (int) hmbkp_is_in_progress();
+	exit;
+}
+add_action( 'wp_ajax_hmbkp_is_in_progress', 'hmbkp_ajax_is_backup_in_progress' );
+
+function hmbkp_ajax_calculate_backup_size() {
+	echo hmbkp_calculate();
+	exit;
+}
+add_action( 'wp_ajax_hmbkp_calculate', 'hmbkp_ajax_calculate_backup_size' );
