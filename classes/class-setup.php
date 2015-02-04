89d88922c472ea59d6c922fe44c8e64785e0492f
pauldewouters <pauldewouters@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-04 09:34:22 +0000

    Version 3.1.2
    git-svn-id: https://plugins.svn.wordpress.org/backupwordpress/trunk@1082153 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- classes/class-setup.php
+++ classes/class-setup.php
@@ -1,11 +1,11 @@
 <?php
 
-defined( 'ABSPATH' ) or exit;
+namespace HM\BackUpWordPress;
 
 /**
  * Class BackUpWordPress_Setup
  */
-class BackUpWordPress_Setup {
+class Setup {
 
 	/**
 	 * Defines the minimum version of WordPress required by BWP.
@@ -51,27 +51,14 @@ class BackUpWordPress_Setup {
 			return;
 		}
 
-		// Determine if we need to do any cleanup
-		if ( ! class_exists( 'HMBKP_Schedules' ) ) {
-			return;
+		// Delete Cron schedules.
+		global $wpdb;
+
+		$schedules = $wpdb->get_col( $wpdb->prepare( "SELECT option_name FROM $wpdb->options WHERE option_name LIKE %s", 'hmbkp_schedule_%' ) );
+
+		foreach ( array_map( function( $item ){ return ltrim( $item, 'hmbkp_schedule_' ); }, $schedules ) as $item ) {
+			wp_clear_scheduled_hook( 'hmbkp_schedule_hook', array( 'id' => $item ) );
 		}
-
-		$schedules = HMBKP_Schedules::get_instance();
-
-		if ( empty( $schedules ) ) {
-			return;
-		}
-
-		// Clear schedule crons
-		foreach ( $schedules->get_schedules() as $schedule ) {
-			$schedule->unschedule();
-		}
-
-		// Opt them out of support
-		delete_option( 'hmbkp_enable_support' );
-
-		// Remove the directory filesize cache
-		delete_transient( 'hmbkp_directory_filesizes' );
 
 	}
 
@@ -139,7 +126,7 @@ class BackUpWordPress_Setup {
 	 */
 	public static function display_admin_notices() {
 
-		echo '<div class="error"><p>' . self::get_notice_message() . '</p></div>';
+		echo '<div class="error"><p>' . esc_html( self::get_notice_message() ) . '</p></div>';
 
 	}
 
